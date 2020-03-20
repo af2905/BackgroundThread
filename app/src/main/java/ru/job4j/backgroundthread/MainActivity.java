edd05e46e7e83249c137fbed252d1fec13f92617
af2905 <af2905g@gmail.com>
2020-03-20 22:58:54 +0300

    thread execution example

---
+++ app/src/main/java/ru/job4j/backgroundthread/MainActivity.java
@@ -0,0 +1,101 @@
+package ru.job4j.backgroundthread;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import ru.job4j.samplebackgroundthread.R;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "log";
+    private volatile boolean stopThread;
+    TextView text;
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+        image = findViewById(R.id.image);
+        LoadImageFromNetwork load = new LoadImageFromNetwork();
+        new Thread(load).start();
+    }
+
+    public void startThread(View view) {
+        TestRunnable runnable = new TestRunnable(10);
+        new Thread(runnable).start();
+    }
+
+    public void stopThread(View view) {
+        if (!stopThread) {
+            stopThread = true;
+        }
+        Toast.makeText(this, "Stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    class TestRunnable implements Runnable {
+        private int times;
+
+        TestRunnable(int times) {
+            this.times = times;
+        }
+
+        @Override
+        public void run() {
+            int count = 0;
+            while (count != times) {
+                if (count < 3) {
+                    runOnUiThread(() -> text.setText("We consider your final price ... "));
+                }
+
+                if (count == 5) {
+                    runOnUiThread(() -> {
+                        int sale = (int) (Math.random() * 100);
+                        text.setText("final PRICE for You " + sale + "% off");
+                    });
+                }
+                Log.d(TAG, "startThread: " + count);
+                count++;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (stopThread) {
+                    stopThread = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    private Bitmap loadImageFromNetwork(String url) {
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+    class LoadImageFromNetwork implements Runnable {
+        @Override
+        public void run() {
+            final Bitmap bitmap = loadImageFromNetwork(
+                    "https://secure-static.schutz.com.br/medias/sys_master/images/h65/he7/h00/h00/9140293435422/Header-Sale.jpg");
+            runOnUiThread(() -> image.setImageBitmap(bitmap));
+        }
+    }
+}
